788c5360e659f8c38d728ff49958289508b4d56a
Romain Beguet <beguet@adacore.com>
2020-10-24 16:28:18 +0200

    Implement physically-based shading.

--- src/glsl/frag.glsl
+++ src/glsl/frag.glsl
@@ -7,8 +7,47 @@ const float epsilon = 0.001f;
 const float min_step_size = 0.005f;
 const float max_dist = 20;
 const int max_steps = 300;
+const float PI = 3.1415926535897932;
 
 varying vec4 pos;
+
+vec3 fresnelSchlick(float cosTheta, vec3 F0) {
+    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
+}
+
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a      = roughness*roughness;
+    float a2     = a*a;
+    float NdotH  = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
+
+    float num   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return num / denom;
+}
+
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r*r) / 8.0;
+
+    float num   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return num / denom;
+}
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
 
 //sphere
 
@@ -59,6 +98,8 @@ struct Primitive {
 
    vec3 color;
    float mirror;
+   float metallic;
+   float roughness;
 };
 
 float dist_to_primitive(vec3 x, Primitive prim) {
@@ -85,15 +126,15 @@ vec3 primitive_normal(vec3 x, Primitive prim) {
 
 #define PRIM_COUNT 8
 const Primitive prims[PRIM_COUNT] = Primitive[](
-   Primitive(SPHERE, vec3(0.5, 0.5, 1.0), 1.0, vec3(1, 0, 0), 0),
-   Primitive(CUBE,   vec3(-0.5, 0, 2.0),  0.8, vec3(0, 0, 1), 0),
+   Primitive(SPHERE, vec3(0.5, 0.5, 1.0), 1.0, vec3(1, 0, 0), 0, 0.1, 0.1),
+   Primitive(CUBE,   vec3(-0.5, 0, 2.0),  0.8, vec3(0, 0, 1), 0, 0.8, 0.8),
 
-   Primitive(PLANE, vec3(0, 1, 0),  1.0, vec3(0.5, 0.5, 0.5), 0),
-   Primitive(PLANE, vec3(0, -1, 0), 5.0, vec3(0.5, 0.5, 0.5), 0),
-   Primitive(PLANE, vec3(1, 0, 0),  5.0, vec3(0.5, 0.5, 0.5), 0),
-   Primitive(PLANE, vec3(-1, 0, 0), 5.0, vec3(0.5, 0.5, 0.5), 0),
-   Primitive(PLANE, vec3(0, 0, 1),  5.0, vec3(0.5, 0.5, 0.5), 0),
-   Primitive(PLANE, vec3(0, 0, -1), 5.0, vec3(0.5, 0.5, 0.5), 0)
+   Primitive(PLANE, vec3(0, 1, 0),  1.0, vec3(0.5, 0.5, 0.5), 0, 0, 0.6),
+   Primitive(PLANE, vec3(0, -1, 0), 5.0, vec3(0.5, 0.5, 0.5), 0, 0, 0.6),
+   Primitive(PLANE, vec3(1, 0, 0),  5.0, vec3(0.5, 0.5, 0.5), 0, 0, 0.6),
+   Primitive(PLANE, vec3(-1, 0, 0), 5.0, vec3(0.5, 0.5, 0.5), 0, 0, 0.6),
+   Primitive(PLANE, vec3(0, 0, 1),  5.0, vec3(0.5, 0.5, 0.5), 0, 0, 0.6),
+   Primitive(PLANE, vec3(0, 0, -1), 5.0, vec3(0.5, 0.5, 0.5), 0, 0, 0.6)
 );
 
 float closest_primitive(vec3 x, out int index) {
@@ -146,28 +187,59 @@ bool raycast(vec3 from, vec3 dir, out int index, out vec3 coll) {
    return false;
 }
 
-vec3 shade(vec3 pos, vec3 color, vec3 normal, vec3 light_pos) {
-   // shadow cast
+vec3 shade(vec3 pos, vec3 normal, vec3 dir, vec3 light_pos,
+           vec3 albedo, float metallic, float roughness) {
+
+   // reflectance
+   vec3 N = normal;
+   vec3 V = -dir;
+
+   vec3 F0 = mix(vec3(0.04), albedo, metallic);
+
+   vec3 Lo = vec3(0.0);
+
+   // do for each light:
    vec3 light_dir = light_pos - pos;
    float light_distance = length(light_dir);
-   vec3 shadow_ray_dir = light_dir / light_distance;
-   vec3 shadow_ray_pos = pos + normal * min_step_size * 5;
+   vec3 L = light_dir / light_distance;
+   vec3 H = normalize(V + L);
+
+   // calculate light radiance
+   float attenuation = 1.0; // / (light_distance * light_distance);
+   vec3 radiance     = vec3(0.9, 0.9, 0.8) * attenuation;
+
+   // cook-torrance BRDF
+   float NDF = DistributionGGX(N, H, roughness);
+   float G   = GeometrySmith(N, V, L, roughness);
+   vec3  F   = fresnelSchlick(max(dot(H, V), 0.0), F0);
+
+   vec3 kS = F;
+   vec3 kD = vec3(1.0) - kS;
+   kD *= 1.0 - metallic;
+
+   vec3 numerator    = NDF * G * F;
+   float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
+   vec3 specular     = numerator / max(denominator, 0.001);
+
+   // shadow cast
 
    float shadows = softshadows(
-      shadow_ray_pos,
-      shadow_ray_dir,
+      pos + normal * min_step_size * 5,
+      L,
       min_step_size,
       light_distance,
       64
    );
 
-   // lambert
-   float NoL = max(dot(normal, shadow_ray_dir), 0.0);
-   vec3 LDirectional = vec3(0.9, 0.9, 0.8) * NoL;
-   vec3 LAmbient = vec3(0.03, 0.04, 0.1);
-   vec3 diffuse = color * (LDirectional + LAmbient);
+   // add to outgoing radiance Lo
+   float NdotL = max(dot(N, L), 0.0);
+   Lo += (kD * albedo / PI + specular) * radiance * NdotL * mix(0.1, 1, shadows);
 
-   return mix(diffuse * 0.1, diffuse, shadows);
+   // finally
+   vec3 ambient = vec3(0.03, 0.04, 0.1) * albedo * 0.1;
+   Lo += ambient;
+
+   return Lo + ambient;
 }
 
 vec3 fog(vec3 from, vec3 pos, vec3 col, vec3 bg) {
@@ -182,9 +254,11 @@ vec3 pixel_color_0(vec3 from, vec3 dir, vec3 light_pos) {
    int prim_index;
    vec3 pos;
    if (raycast(from, dir, prim_index, pos)) {
-      vec3 color = prims[prim_index].color;
+      vec3 albedo = prims[prim_index].color;
+      float metallic = prims[prim_index].metallic;
+      float roughness = prims[prim_index].roughness;
       vec3 normal = primitive_normal(pos, prims[prim_index]);
-      vec3 result = shade(pos, color, normal, light_pos);
+      vec3 result = shade(pos, normal, dir, light_pos, albedo, metallic, roughness);
       return fog(from, pos, result, background_color);
 
    }
@@ -197,9 +271,11 @@ vec3 pixel_color_1(vec3 from, vec3 dir, vec3 light_pos) {
    int prim_index;
    vec3 pos;
    if (raycast(from, dir, prim_index, pos)) {
-      vec3 color = prims[prim_index].color;
+      vec3 albedo = prims[prim_index].color;
+      float metallic = prims[prim_index].metallic;
+      float roughness = prims[prim_index].roughness;
       vec3 normal = primitive_normal(pos, prims[prim_index]);
-      vec3 result = shade(pos, color, normal, light_pos);
+      vec3 result = shade(pos, normal, dir, light_pos, albedo, metallic, roughness);
 
       // reflection
 
@@ -227,9 +303,11 @@ vec3 pixel_color_2(vec3 from, vec3 dir, vec3 light_pos) {
    int prim_index;
    vec3 pos;
    if (raycast(from, dir, prim_index, pos)) {
-      vec3 color = prims[prim_index].color;
+      vec3 albedo = prims[prim_index].color;
+      float metallic = prims[prim_index].metallic;
+      float roughness = prims[prim_index].roughness;
       vec3 normal = primitive_normal(pos, prims[prim_index]);
-      vec3 result = shade(pos, color, normal, light_pos);
+      vec3 result = shade(pos, normal, dir, light_pos, albedo, metallic, roughness);
 
       // reflection
 
@@ -258,8 +336,6 @@ void main(void)
    vec3 light_pos = vec3(cos(time), 2, sin(time));
    vec3 initial_pos = frag_pos + camera_position;
 
-   gl_FragColor = vec4(
-      pow(pixel_color_1(initial_pos, dir, light_pos), vec3(0.4545)),
-      1
-   );
+   vec3 col = pixel_color_1(initial_pos, dir, light_pos);
+   gl_FragColor = vec4(pow(col / (col + vec3(1.0)), vec3(0.4545)), 1);
 }
