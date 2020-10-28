5365005a7f8e0f2c8680dbbb189b562daf3d8396
Romain Beguet <beguet@adacore.com>
2020-10-28 17:23:17 +0100

    Simulate infinite bounces.

--- src/glsl/compute_probe_radiance.glsl
+++ src/glsl/compute_probe_radiance.glsl
@@ -15,7 +15,7 @@ void main(void) {
    vec3 ray_dir = ray_id_to_ray_dir(ray_id);
 
    vec3 light_pos = vec3(3.0 + cos(time), 2, sin(time));
-   vec3 col = pixel_color_direct(world_position, ray_dir, light_pos);
+   vec3 col = pixel_color_irradiance_probes(world_position, ray_dir, light_pos);
 
    gl_FragColor = vec4(col, 1);
 }
