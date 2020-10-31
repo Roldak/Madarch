94e66343e1e7fc5d00f3f66bbda9874c80da8c98
Romain Beguet <beguet@adacore.com>
2020-10-31 20:51:42 +0100

    Move Load_Shader subprogram.

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,3 @@
---  part of OpenGLAda, (c) 2017 Felix Krause
---  released under the terms of the MIT license, see the file "COPYING"
-
 with Ada.Text_IO;
 
 with GL.Buffers;
@@ -19,8 +16,7 @@ with GLFW_Utils;
 
 with Glfw.Input.Keys;
 
-with GNATCOLL.VFS;
-with GNATCOLL.Strings;
+with Load_Shader;
 
 procedure Main is
    use GL;
@@ -28,64 +24,6 @@ procedure Main is
    use GL.Types;
    use GL.Fixed.Matrix;
    use GL.Immediate;
-
-   procedure Load_Shader
-     (Shader : in out GL.Objects.Shaders.Shader;
-      Source_File : String)
-   is
-      use GNATCOLL;
-
-      function Load_Source (Path : VFS.Virtual_File) return Strings.XString;
-
-      procedure Resolve_Include_Directives
-        (Source_Dir : VFS.Virtual_File;
-         Content    : in out Strings.XString;
-         From       : Natural := 1)
-      is
-         Next_Include        : Natural := Content.Find ("#include", From);
-         Included_File_Start : Natural := Content.Find('"', Next_Include + 1);
-         Included_File_End   : Natural := Content.Find('"', Included_File_Start + 1);
-      begin
-         if Next_Include /= 0 then
-            declare
-               File_Name : Strings.XString := Content.Slice
-                  (Included_File_Start + 1, Included_File_End - 1);
-
-               File_Path : VFS.Virtual_File := VFS.Create_From_Dir
-                 (Source_Dir, VFS.Filesystem_String (File_Name.To_String));
-            begin
-               Content.Replace_Slice
-                 (Next_Include,
-                  Included_File_End,
-                  Load_Source (File_Path));
-
-               Resolve_Include_Directives
-                 (Source_Dir, Content, Included_File_End);
-            end;
-         end if;
-      end Resolve_Include_Directives;
-
-      function Load_Source (Path : VFS.Virtual_File) return Strings.XString is
-         Content : Strings.XString := VFS.Read_File (Path);
-      begin
-         Resolve_Include_Directives (Path.Dir, Content);
-         return Content;
-      end Load_Source;
-
-      Source_Path : VFS.Virtual_File :=
-         VFS.Create (VFS.Filesystem_String (Source_File));
-   begin
-
-      Shader.Initialize_Id;
-      GL.Objects.Shaders.Set_Source
-        (Shader, Load_Source (Source_Path).To_String);
-      Shader.Compile;
-
-      if not Shader.Compile_Status then
-         Ada.Text_IO.Put_Line ("Compilation of " & Source_File & " failed. log:");
-         Ada.Text_IO.Put_Line (Shader.Info_Log);
-      end if;
-   end Load_Shader;
 
    procedure List_Shaders (Program : GL.Objects.Programs.Program) is
    begin
