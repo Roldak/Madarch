83eb4a32861a888d277a29f5a7b572aea69056a8
Romain Beguet <beguet@adacore.com>
2020-12-04 19:37:48 +0100

    fix prims.

--- madarch/madarch-primitives-spheres.ads
+++ madarch/madarch-primitives-spheres.ads
@@ -10,17 +10,17 @@ package Madarch.Primitives.Spheres is
    Center : constant Component := Create ("center", Vector3_Kind);
    Radius : constant Component := Create ("radius", Float_Kind);
 
-   function Distance (S : Struct_Expr; P : Expr'Class) return Expr'Class is
+   function Get_Distance (S : Struct_Expr; P : Expr'Class) return Expr'Class is
      (S.Get (Center)."-" (P).Length - S.Get (Radius));
 
-   function Normal (S : Struct_Expr; P : Expr'Class) return Expr'Class is
+   function Get_Normal (S : Struct_Expr; P : Expr'Class) return Expr'Class is
      (P."-" (S.Get (Center)).Normalize);
 
-   Sphere : Primitive := Create
+   Sphere : constant Primitive := Create
      ("Sphere",
       (Center, Radius, Material_Id),
-      Distance'Access,
-      Normal'Access,
+      Get_Distance'Access,
+      Get_Normal'Access,
       Get_Material_Id'Access);
 
    function Create
