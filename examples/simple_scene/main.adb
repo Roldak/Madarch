2cc32812c673eba8de8a6540fd51005fa6c0819b
Romain Beguet <beguet@adacore.com>
2020-12-05 18:12:03 +0100

    Setup room scene.

--- examples/simple_scene/main.adb
+++ examples/simple_scene/main.adb
@@ -32,29 +32,43 @@ procedure Main is
 
    Renderer : Renderers.Renderer := Renderers.Create (Window, Scene);
 
-   Red_Mat : Entities.Entity :=
-      Materials.Create ((0.9, 0.1, 0.1), 0.1, 0.9);
-
    Sphere_Instance : Entities.Entity :=
-      Primitives.Spheres.Create ((1.0, 1.0, 2.0), 1.0, 0);
-
-   Plane_Instance : Entities.Entity :=
-      Primitives.Planes.Create ((0.0, 1.0, 0.0), 1.0, 0);
+      Primitives.Spheres.Create ((3.5, 3.0, 3.0), 1.0, 3);
 
    Point_Light_Instance : Entities.Entity :=
       Lights.Point_Lights.Create ((0.0, 3.0, 0.0), (0.9, 0.9, 0.9));
 
    Time : Single := 0.0;
 
-   Dist : Single := Primitives.Eval_Dist
-     (Primitives.Spheres.Sphere, Sphere_Instance, (0.0, 0.0, 0.0));
-begin
-   Ada.Text_IO.Put_Line (Dist'Image);
+   Prim_Count : Natural := 1;
 
-   Renderers.Set_Material (Renderer, 1, Red_Mat);
-   Renderers.Set_Primitive (Renderer, 1, Primitives.Spheres.Sphere, Sphere_Instance);
-   Renderers.Set_Primitive (Renderer, 1, Primitives.Planes.Plane, Plane_Instance);
+   Planes : Entities.Entity_Array :=
+     (Primitives.Planes.Create ((0.0,  1.0, 0.0), 1.0, 0),
+      Primitives.Planes.Create ((0.0, -1.0, 0.0), 7.0, 0),
+      Primitives.Planes.Create (( 1.0, 0.0, 0.0), 1.0, 1),
+      Primitives.Planes.Create ((-1.0, 0.0, 0.0), 7.0, 2),
+      Primitives.Planes.Create ((0.0, 0.0,  1.0), 6.0, 0),
+      Primitives.Planes.Create ((0.0, 0.0, -1.0), 7.0, 0));
+begin
+   for Plane of Planes loop
+      Renderers.Set_Primitive (Renderer, Prim_Count, Primitives.Planes.Plane, Plane);
+      Prim_Count := Prim_Count + 1;
+   end loop;
+
+   Renderers.Set_Material
+     (Renderer, 1, Materials.Create ((0.0, 0.0, 0.0), 0.0, 0.6));
+   Renderers.Set_Material
+     (Renderer, 2, Materials.Create ((1.0, 0.0, 0.0), 0.0, 0.6));
+   Renderers.Set_Material
+     (Renderer, 3, Materials.Create ((0.0, 0.0, 1.0), 0.0, 0.6));
+   Renderers.Set_Material
+     (Renderer, 4, Materials.Create ((0.1, 0.1, 0.1), 0.9, 0.1));
+
+   Renderers.Set_Primitive
+     (Renderer, Prim_Count, Primitives.Spheres.Sphere, Sphere_Instance);
+
    Renderers.Set_Light (Renderer, 1, Lights.Point_Lights.Point_Light, Point_Light_Instance);
+
    while Window.Is_Opened loop
       Renderers.Render (Renderer);
       Window.Poll_Events;
